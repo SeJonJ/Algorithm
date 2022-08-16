0198c0e3a7b7bbbac29d8a07cc0f11d760c48b25
SeJonJ <wkdtpwhs@gmail.com>
2022-08-16 23:48:40 +0900

    7568 풀이 완료

--- src/baekJoon/Quiz_7568.java
+++ src/baekJoon/Quiz_7568.java
@@ -3,50 +3,66 @@ package baekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Quiz_7568 {
-    static int[] arr;
-    static PriorityQueue<Dungchi> q = new PriorityQueue<Dungchi>(Collections.reverseOrder());
+
+    static ArrayList<Dungchi> arr = new ArrayList<>();
+    static int[] rank;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        arr = new int[n];
+        rank = new int[n];
+        Arrays.fill(rank, 1);
 
         for(int i=0; i<n; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
-            q.offer(new Dungchi(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), i));
+            arr.add(new Dungchi(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 
         }
 
-        while (!q.isEmpty()) {
-            Dungchi d = q.poll();
-            System.out.println(d.height + " " + d.weight + " " + d.num);
+        for (int i = 0; i<n; i++){
+            Dungchi d = arr.get(i);
+
+            for(int j=i+1; j<n; j++){
+                if (d.compareTo(arr.get(j)) == -1) {
+                    // i 번째 사람과 j 번째 사람을 비교했을 때 j 번째 사람이 i 번째보다 큰 경우
+                    rank[i]++;
+                } else if (d.compareTo(arr.get(j)) == 1) {
+                    // i 번째 사람과 j 번재 사람을 비교했을 때 i 번째 사람이 j 번째보다 큰 경우
+                    rank[j]++;
+                }
+            }
         }
+
+        for (int i : rank) {
+            System.out.print(i + " ");
+        }
+
     }
 }
 
 class Dungchi implements Comparable<Dungchi>{
     int weight;
     int height;
-    int num;
 
-    Dungchi(int weight, int height, int num) {
+    Dungchi(int weight, int height) {
         this.weight = weight;
         this.height = height;
-        this.num = num;
     }
 
     @Override
     public int compareTo(Dungchi o) {
-        if (this.weight != o.weight) {
-            return this.height-o.height;
+        if (this.weight > o.weight && this.height > o.height) {
+            return 1;
+        } else if (this.weight < o.weight && this.height < o.height) {
+            return -1;
         }else{
-            return o.height-this.height;
+            return 0;
         }
+
     }
 }
